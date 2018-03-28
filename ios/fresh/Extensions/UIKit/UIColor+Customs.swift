24fe961735888cabf35c4359eccfcc5b3a9a2633
hoonhoonhoon <hoonhoon@woowahan.com>
2018-03-28 09:35:15 +0900

    new file:   ios/fresh/Extensions/UIKit/UIColor+Customs.swift 	new file:   ios/fresh/Extensions/UIKit/UIColor.swift

---
+++ ios/fresh/Extensions/UIKit/UIColor+Customs.swift
@@ -0,0 +1,40 @@
+//
+//  ConfigColor.swift
+//  BaeminFresh
+//
+//  Created by hoonhoon on 2017. 4. 28..
+//  Copyright © 2017년 woowahan. All rights reserved.
+//
+
+import Foundation
+
+extension UIColor {
+
+    static let paleGrey = #colorLiteral(red: 0.9098039216, green: 0.9254901961, blue: 0.9411764706, alpha: 1)
+    static let subMenuTitleNormal = #colorLiteral(red: 0.3490196078, green: 0.3490196078, blue: 0.3490196078, alpha: 1)
+
+    // 메인메뉴
+    static let mainMenuTitleNormal = #colorLiteral(red: 0.137254902, green: 0.137254902, blue: 0.137254902, alpha: 1)
+
+    // 상품
+    static let productTitle = #colorLiteral(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)
+    static let productShortDescription = #colorLiteral(red: 0.6039215686, green: 0.6039215686, blue: 0.6039215686, alpha: 1)
+    static let retailPrice = #colorLiteral(red: 0.7490196078, green: 0.7490196078, blue: 0.7490196078, alpha: 1)
+    static let 더보기 = #colorLiteral(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)
+    static let discountPercentageBackground = #colorLiteral(red: 0.9843137255, green: 0.4039215686, blue: 0.4039215686, alpha: 0.9)
+    static let soldOut = #colorLiteral(red: 0.6039215686, green: 0.6039215686, blue: 0.6039215686, alpha: 1)
+
+    // 색깔 스타일 정의
+    static let white = UIColor(rgba: "#ffffff")
+    static let mint = UIColor(rgba: "#2ac1bc")
+    static let black00 = UIColor(rgba: "#000000")
+    static let black20 = UIColor(rgba: "#333333")
+    static let grey30 = UIColor(rgba: "#4d4d4d")
+    static let grey45 = UIColor(rgba: "#737373")
+    static let grey60 = UIColor(rgba: "#999999")
+    static let grey75 = UIColor(rgba: "#bfbfbf")
+    static let grey93 = UIColor(rgba: "#ededed")
+    static let grey95 = UIColor(rgba: "#f2f2f2")
+	static let greyrishBrown = UIColor(rgba: "#4c453c")
+    
+}
